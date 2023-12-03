9a9fa38232d5eb7188cc4c9604af0526aa8a3077
Youngjeong Choi <doe977@naver.com>
2023-12-04 04:21:21 +0900

    abstracttest

--- app/src/main/java/com/example/calculator_re/Calculate.kt
+++ app/src/main/java/com/example/calculator_re/Calculate.kt
@@ -1,8 +1,8 @@
 package com.example.calculator_re
 
 
-class Calculate(val operator: AbstractOperation){
-    fun operate(num1: Int, num2: Int): Double {
+open class Calculate(val operator: AbstractOperation) {
+    open fun operate(num1: Int, num2: Int): Double {
         return operator.operate(num1, num2)
     }
 }
